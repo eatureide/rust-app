7aeb186cef212021ccc30ac8e44c6e7092af7cbd
ZeJun.Yi <zejun.yi@watsons.com.cn>
2023-04-29 12:02:08 +0800

    11

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,18 +1,21 @@
-const MAX_POINT: u32 = 100_000;
+// const MAX_POINT: u32 = 100_000;
 
 fn main() {
     // println!("Hello, world!");
     // const MAX_POINT: u32 = 100_000;
 
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
+    // let x = 5;
+    // let x = x + 1;
+    // let x = x * 2;
 
-    let spaces_str = "    ";
-    let spaces_num = spaces_str.len();
+    // let spaces_str = "    ";
+    // let spaces_num = spaces_str.len();
 
-    print!("{}", spaces_num);
+    // print!("{}", spaces_num);
 
     // println!("the value ofx is {}", x);
     // println!("const is {}", MAX_POINT)
+
+    let guess: u32 = "42".parse().expect("not a number");
+    println!("{}", guess);
 }
