a7326d75e4ea55c952c65b822858722a64f5e5e3
ZeJun.Yi <zejun.yi@watsons.com.cn>
2023-04-30 20:54:43 +0800

    test

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -8,12 +8,34 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.length
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.length > other.width
+    }
+
+    fn squre(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            length: size,
+        }
+    }
 }
 
 fn main() {
-    let rect = Rectangle {
+    let s = Rectangle::squre(20);
+    
+    let rect1 = Rectangle {
         width: 30,
         length: 50,
     };
-    println!("{:?}", rect.area());
+    let rect2 = Rectangle {
+        width: 10,
+        length: 40,
+    };
+    let rect3 = Rectangle {
+        width: 35,
+        length: 55,
+    };
+    println!("{:?}", rect1.can_hold(&rect2));
+    println!("{:?}", rect2.can_hold(&rect3));
 }
